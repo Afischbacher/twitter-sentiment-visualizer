b851b5e497a3286b42cf2d2b3a65e8f705a778b9
Afischbacher <ital-andre95@live.com>
2017-03-13 01:16:57 -0400

    added a stylesheet, updated the front-end and the back-end as well

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-/**
- * Created by ital- on 2017-03-02.
- */
 var express = require("express");
 
 var app = express();
 
 app.use(express.static('public'));
 
+app.get('/results/', function (req, res) {
+    var array = [];
+    array.push(req.query.q);
+    res.send(JSON.stringify(array));
+});
 app.listen(3000, function () {
     console.log("Listening app on port 3000! ");
 });
--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -1,6 +1,14 @@
 /**
  * Created by ital- on 2017-03-02.
  */
-app.controller("main-ctrl", ["$scope", function ($scope) {
-            $scope.title = "Twitter Sentiment Visualizer";
+app.controller("main-ctrl", ["$scope","$http", function ($scope, $http) {
+    $scope.title = "Twitter Sentiment Visualizer";
+    $scope.results = [];
+
+
+    $scope.getResponse = function () {
+        $http.get("/results/?q="+$scope.searchInput).then(function (data) {
+            $scope.results = data.data;
+        });
+    }
 }]);
