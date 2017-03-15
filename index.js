10aca5deedec3825d58d34eb66a04ba483f9451e
Afischbacher <ital-andre95@live.com>
2017-03-15 00:07:45 -0400

    added various npm dependencies, updated the .gitignore for the environment variables

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require("express");
+var twitter = require("twitter");
+var dotenv = require("dotenv");
 
+dotenv.config();
 var app = express();
+
+var twitterApi = new twitter({
+    consumer_key: process.env.Consumer_Key_Twitter,
+    consumer_secret: process.env.Consumer_Secret_Twitter,
+    access_token_key: process.env.Access_Token_Twitter,
+    access_token_secret: process.env.Access_Token_Secret_Twitter
+});
 
 app.use(express.static('public'));
 
@@ -9,6 +19,9 @@ app.get('/results/', function (req, res) {
     array.push(req.query.q);
     res.send(JSON.stringify(array));
 });
+
+
 app.listen(3000, function () {
     console.log("Listening app on port 3000! ");
 });
+
