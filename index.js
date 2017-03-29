df3fd50ad933bd619ec31465d741904cc3c8e077
Afischbacher <ital-andre95@live.com>
2017-03-28 21:52:11 -0400

    Changed the port for index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/results/:query', function (req, res) {
 });
 
 
-app.listen(3000, function () {
-    console.log("Listening to the app on port 3000! ");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Listening to the app on port" + process.env.PORT);
 });
 
